cc7451301e12b884506d372692be5e1f9deab68e
yoshi-app <admin@aiyoshiapp.com>
2026-05-20 05:00:40 +0000

    Add IGW setup command

--- setup.sh
+++ setup.sh
@@ -12,3 +12,15 @@ aws --profile localstack --endpoint-url=http://localhost:4566 ec2 create-subnet 
 
 #プライベートサブネット作成
 aws --profile localstack --endpoint-url=http://localhost:4566 ec2 create-subnet --vpc-id vpc-59c000a31a31857fb --cidr-block 10.0.2.0/24 --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=sensei-private-subnet}]'
+
+#IGW作成
+aws --profile localstack --endpoint-url=http://localhost:4566 ec2 create-internet-gateway
+
+#IGWをVPCにアタッチ
+aws --profile localstack --endpoint-url=http://localhost:4566 ec2 attach-internet-gateway --internet-gateway-id igw-2eaa4341363ae7d4a --vpc-id vpc-59c000a31a31857fb
+
+#ルートテーブルにIGWへの経路を追加
+aws --profile localstack --endpoint-url=http://localhost:4566 ec2 create-route --route-table-id rtb-17c3d6a9a44b2b927 --destination-cidr-block 0.0.0.0/0 --gateway-id igw-2eaa4341363ae7d4a
+
+#パブリックサブネットにルートテーブルを関連付け
+aws --profile localstack --endpoint-url=http://localhost:4566 ec2 associate-route-table --route-table-id rtb-17c3d6a9a44b2b927 --subnet-id subnet-f1446cf9d06f191c4
